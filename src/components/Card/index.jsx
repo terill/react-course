ed7dc5945df217c117c29386d6938c6a2074ce20
Dmitry Mikhalkov <terilloff@gmail.com>
2021-02-09 13:04:47 +0300

    Understanding the Basics - Task2

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 function Card(props) {
+  const [cardState, setCardState] = useState({ checked: false });
+
+  const handleCheckboxChange = () => {
+    setCardState({ checked: !cardState.checked });
+  };
+
   return (
-    <div className="Card">
+    <div className={'Card ' + (cardState.checked ? 'checked' : '')}>
       <h2>{props.caption}</h2>
       <hr />
       <p>{props.children}</p>
+      <input onChange={handleCheckboxChange} type="checkbox" />
     </div>
   );
 }
