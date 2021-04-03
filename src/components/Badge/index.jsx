31d052b4ddbf0bc30f455ea053e30c257430dd4d
Dmitry Mikhalkov <terilloff@gmail.com>
2021-04-03 12:10:42 +0300

    Redux - Task13 - Fixes

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import './index.css';
 
-const Badge = props => {
-  const cardsAmount = props.cardsAmount;
+const Badge = () => {
+  const cardsAmount = useSelector(state => state.cards.length);
 
   return (
     <button className="Badge">
@@ -13,10 +13,4 @@ const Badge = props => {
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    cardsAmount: state.cards.length
-  };
-};
-
-export default connect(mapStateToProps)(Badge);
+export default Badge;
